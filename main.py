44059898fa75c5434f811f3dc54ed6b846da68d1
Che100r <spirrus1999@mail.ru>
2022-09-20 10:15:28 +0300

    git init

---
+++ main.py
@@ -0,0 +1,22 @@
+def Task1():
+    print("Task 1")
+
+    src = "HE FDFLLO342555*^F$%"
+
+    if src.isupper():
+        print("YES")
+    else:
+        print("NO")
+
+
+def Task2():
+    print("Task 2")
+
+    name = "First"
+    surname = "Second"
+
+    print(name[0] + "." + surname[0] + ".")
+
+
+
+Task2()
